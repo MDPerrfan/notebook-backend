027548b375a8fa92ebcf0bf0a5a8be29f4c59de4
Mohammed Parves <mdperrfan@gmail.com>
2024-05-21 17:28:09 +0600

    connected with mongoDB

---
+++ db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+async function connectToMongo() {
+    try {
+        await mongoose.connect('mongodb://localhost:27017', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('Connected to MongoDB');
+    } catch (err) {
+        console.error('Failed to connect to MongoDB:', err);
+    }
+}
+
+module.exports = connectToMongo;
