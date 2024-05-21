9a3227f8c78fab5ad452f2a1cb76d47893b9740f
Mohammed Parves <mdperrfan@gmail.com>
2024-05-21 20:51:20 +0600

    Added models and router

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connectToMongo() {
     try {
-        await mongoose.connect('mongodb://localhost:27017', {
+        await mongoose.connect('mongodb://localhost:27017/', {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
