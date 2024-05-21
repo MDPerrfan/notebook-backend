ce088af41e92a7f2f29407bfbcfa329af0559ed5
Mohammed Parves <mdperrfan@gmail.com>
2024-05-21 21:50:44 +0600

    Updated user validation and sending data to database

--- models/User.js
+++ models/User.js
@@ -19,4 +19,6 @@ const userSchema = new Schema({
         default: Date.now
     }
 });
-module.exports = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
+
+module.exports = User
--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,28 @@
 const express = require('express');
+const { body, validationResult } = require('express-validator');
 const User = require('../models/User');
 const router = express.Router();
 
-router.post('/', async(req, res) => {
-    try {
-        const user = new User(req.body);
-        await user.save();
-        res.status(201).json(user);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
+router.post(
+    '/', [
+        body('name').notEmpty().withMessage('Name is required'),
+        body('email').isEmail().withMessage('Invalid email address'),
+        body('password').isLength({ min: 5 }).withMessage('Password must be at least 5 characters long')
+    ],
+    async(req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        try {
+            const user = new User(req.body);
+            await user.save();
+            res.status(201).json(user);
+        } catch (error) {
+            res.status(400).json({ error: error.message });
+        }
     }
-});
+);
 
 module.exports = router;
