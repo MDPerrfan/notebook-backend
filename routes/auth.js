71bdaafe356a0aef3cd44c49dfc3954ae5fdd7e5
Mohammed Parves <mdperrfan@gmail.com>
2024-05-22 11:36:12 +0600

    Changed port, Filtering unique email, updated notes route

--- routes/auth.js
+++ routes/auth.js
@@ -3,12 +3,14 @@ const { body, validationResult } = require('express-validator');
 const User = require('../models/User');
 const router = express.Router();
 
+//Create a User using: POST "/api/auth/createuser". No login required.
 router.post(
-    '/', [
+    '/createuser', [
         body('name').notEmpty().withMessage('Name is required'),
         body('email').isEmail().withMessage('Invalid email address'),
         body('password').isLength({ min: 5 }).withMessage('Password must be at least 5 characters long')
     ],
+
     async(req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
@@ -16,7 +18,11 @@ router.post(
         }
 
         try {
-            const user = new User(req.body);
+            let user = await User.findOne({ email: req.body.email });
+            if (user) {
+                return res.status(400).json({ error: "Email already exists!" })
+            }
+            user = new User(req.body);
             await user.save();
             res.status(201).json(user);
         } catch (error) {
--- routes/notes.js
+++ routes/notes.js
@@ -1,8 +1,24 @@
 const express = require('express');
-
+const { body, validationResult } = require('express-validator');
+const Notes = require('../models/Notes');
 const router = express.Router();
-router.get('/', (req, res) => {
 
-    res.json = []
-})
+//Create a Note using : POST "/api/notes/createnotes" login required
+router.post('/createnotes', [
+        body('title', "Enter a title!"),
+        body('description', "Enter description of the note!")
+    ],
+    async(req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        try {
+            const note = new Notes(req.body)
+            await note.save()
+            res.status(201).json(user);
+        } catch (error) {
+            res.status(400).json({ error: error.message });
+        }
+    });
 module.exports = router;
