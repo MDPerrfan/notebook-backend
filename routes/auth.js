811fa42d6df54896482f307061224c5c8fb8f4df
Mohammed Parves <mdperrfan@gmail.com>
2024-05-22 13:09:53 +0600

    Added JWT token

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const { body, validationResult } = require('express-validator');
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
 const router = express.Router();
 
-//Create a User using: POST "/api/auth/createuser". No login required.
+const JWT_SECRET = "WhatgoesAroundComesAround"
+
+// Create a User using: POST "/api/auth/createuser". No login required.
 router.post(
     '/createuser', [
         body('name').notEmpty().withMessage('Name is required'),
@@ -18,15 +22,35 @@ router.post(
         }
 
         try {
+            // Check whether the user with the same email already exists
             let user = await User.findOne({ email: req.body.email });
             if (user) {
-                return res.status(400).json({ error: "Email already exists!" })
+                return res.status(400).json({ error: "Email already exists!" });
             }
-            user = new User(req.body);
+
+            // Generate salt and hash the password
+            const salt = await bcrypt.genSalt(10);
+            const secPass = await bcrypt.hash(req.body.password, salt);
+
+            // Create a new user with the hashed password
+            user = new User({
+                name: req.body.name,
+                email: req.body.email,
+                password: secPass
+            });
             await user.save();
-            res.status(201).json(user);
+
+            //Generate JWT token
+            const data = {
+                user: {
+                    id: user.id
+                }
+            }
+            const authToken = jwt.sign(data, JWT_SECRET);
+            res.status(201).json({ authToken });
+
         } catch (error) {
-            res.status(400).json({ error: error.message });
+            res.status(500).json({ error: "Internal Server Error" });
         }
     }
 );
